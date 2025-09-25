ef2c06ca67514e02f2ff146ca27d1b193e7d3405
kybometr <83871982+DenisPlyasov@users.noreply.github.com>
2025-09-25 23:43:56 +0300

    Delete schedule directory

--- schedule/states.py
+++
@@ -1,7 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class BotStates(StatesGroup):
-    enter_search = State()   # ждём ввод строки поиска
-    select_item  = State()   # выбор из найденного списка
-    custom_date  = State()   # ввод произвольной даты
-    #fsf
